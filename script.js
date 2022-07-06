40526d5bdb0523518e1acbd3648636caee96b08f
geetanjaliAsati <104821909+geetanjaliAsati@users.noreply.github.com>
2022-07-06 12:15:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const stopwatch = document.getElementById("display");
+
+const playButton = document.getElementById("playButton");
+const pauseButton = document.getElementById("pauseButton");
+const resetButton = document.getElementById("resetButton");
+
+let startTime;
+let elapsedTime = 0;
+let stopWatchInterval;
+
+playButton.addEventListener("click", startStopwatch);
+pauseButton.addEventListener("click", stopStopwatch);
+resetButton.addEventListener("click", resetStopwatch);
+
+function startStopwatch(){
+
+}
+
+function stopStopwatch(){
+
+}
+
+function resetStopwatch(){
+
+}
+
+function timeToString(){
+
+}
+
+function showButton(){
+
+}
